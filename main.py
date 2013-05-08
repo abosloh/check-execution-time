d998dbeab9e7fd0794d699b95499f3039d303004
abosloh <cinema48@gmail.com>
2013-05-08 06:52:31 +0300

    create file

---
+++ main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# coded by : abosloh
+# email : abosloh.654@gmail.com
+# page : https://github.com/abosloh
+
+
+import time
+
+
+start_time = time.time()
+
+print "Welcome"
+
+print time.time() - start_time , "s" # this is the execution time
+
+# if the result : 1.28746032715e-05 s
+# it means : 1.28746032715^-05
